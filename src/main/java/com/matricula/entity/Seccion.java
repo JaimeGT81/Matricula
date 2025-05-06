3c69b3458286efd8d8fc424fae765cb3191b1ccb
Jaime Guerra <jaime.guerra.pe@gmail.com>
2025-05-05 23:20:34 -0500

    test de ci

--- src/main/java/com/matricula/entity/Seccion.java
+++ src/main/java/com/matricula/entity/Seccion.java
@@ -57,4 +57,116 @@ public class Seccion {
     private String modalidad;
 
     // Getters and setters...
+
+    public String getSeccionNRC() {
+        return seccionNRC;
+    }
+
+    public void setSeccionNRC(String seccionNRC) {
+        this.seccionNRC = seccionNRC;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public Docente getDocente() {
+        return docente;
+    }
+
+    public void setDocente(Docente docente) {
+        this.docente = docente;
+    }
+
+    public String getIdAulaDR() {
+        return idAulaDR;
+    }
+
+    public void setIdAulaDR(String idAulaDR) {
+        this.idAulaDR = idAulaDR;
+    }
+
+    public LocalDateTime getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDateTime fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getDiaSemana() {
+        return diaSemana;
+    }
+
+    public void setDiaSemana(String diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public LocalTime getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(LocalTime horaFin) {
+        this.horaFin = horaFin;
+    }
+
+    public LocalDateTime getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDateTime fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getUsuarioRegistro() {
+        return usuarioRegistro;
+    }
+
+    public void setUsuarioRegistro(String usuarioRegistro) {
+        this.usuarioRegistro = usuarioRegistro;
+    }
+
+    public LocalDateTime getFechaUltModificacion() {
+        return fechaUltModificacion;
+    }
+
+    public void setFechaUltModificacion(LocalDateTime fechaUltModificacion) {
+        this.fechaUltModificacion = fechaUltModificacion;
+    }
+
+    public String getUsuarioUltModificacion() {
+        return usuarioUltModificacion;
+    }
+
+    public void setUsuarioUltModificacion(String usuarioUltModificacion) {
+        this.usuarioUltModificacion = usuarioUltModificacion;
+    }
+
+    public Boolean getEstadoSeccion() {
+        return estadoSeccion;
+    }
+
+    public void setEstadoSeccion(Boolean estadoSeccion) {
+        this.estadoSeccion = estadoSeccion;
+    }
+
+    public String getModalidad() {
+        return modalidad;
+    }
+
+    public void setModalidad(String modalidad) {
+        this.modalidad = modalidad;
+    }
 }
